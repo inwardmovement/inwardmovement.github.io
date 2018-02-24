52928c28f5a9d0d8b2c25eddd08ef20e58a9b227
Victor Massé <victor.dhamma@gmail.com>
2018-02-24 23:01:52 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
-var gulp        = require('gulp'),
-    concat      = require('gulp-concat'),
-    minify      = require('gulp-clean-css'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    uglify      = require('gulp-uglify'),
-    exec        = require('child_process').exec,
-    del         = require('del');
+var gulp    = require('gulp'),
+    concat  = require('gulp-concat'),
+    minify  = require('gulp-clean-css'),
+    uglify  = require('gulp-uglify'),
+    exec    = require('child_process').exec,
+    del     = require('del');
 
 gulp.task('default', [ 'clean1', 'css', 'js', 'hugo', 'clean2' ]);
 
@@ -15,9 +14,7 @@ gulp.task('clean1', function(){
 gulp.task('css', function(){
   return gulp.src('static/css/*.css')
     .pipe(concat('main.css'))
-    .pipe(sourcemaps.init())
     .pipe(minify())
-    .pipe(sourcemaps.write())
     .pipe(gulp.dest('static'))
 });
 
