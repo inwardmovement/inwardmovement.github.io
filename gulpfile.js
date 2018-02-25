e67862667af2d8d6e1d7d0f3315dab5ad6844277
Victor Massé <victor.dhamma@gmail.com>
2018-02-25 19:37:57 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('js', function(){
   return gulp.src([
     'static/js/jquery.js',
     'static/js/popper.js',
-    'static/js/bootstrap.min.js',
+    'static/js/bootstrap.js',
     'static/js/style.js'
     ])
     .pipe(concat('main.js'))
