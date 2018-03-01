87fcebd57292523855c9c3ee980e108101a8fecb
Victor Massé <victor.dhamma@gmail.com>
2018-03-01 22:35:19 +0100

    update

--- static/js/app.js
+++ static/js/app.js
@@ -8,11 +8,3 @@ $('#modalDonation')
   .on('hide.bs.modal', function (e) {
     document.querySelector("html").style.overflow = "initial";
   })
-
-/////////////////////////////////////////////////////////////
-// VERSION
-/////////////////////////////////////////////////////////////
-$.getJSON('https://api.github.com/repos/inwardmovement/inwardmovement.github.io/git/refs/heads/source', function(data) {
-  var version = document.getElementById('repo-version');
-  version.innerHTML = data.object.sha.substr(0, 7);
-});
