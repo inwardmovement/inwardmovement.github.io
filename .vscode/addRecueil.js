3c1e1197f5f5f46345be941fcb2e0ac139d2a29b
Victor Massé <victor.dhamma@gmail.com>
2020-10-26 17:30:20 +0100

    remove addRecueil script

--- .vscode/addRecueil.js
+++
@@ -1,7 +0,0 @@
-exports.execute = async (args) => {
-  const vscode = args.require('vscode');
-
-  vscode.window.showInformationMessage(
-    args.replaceValues('Last recueil is ${currentRecueil} and current time is ${currentTime}')
-  );
-};
