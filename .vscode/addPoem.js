eed82c44f4c0647245a3384056167acefa50cca5
Victor Massé <victor.dhamma@gmail.com>
2020-10-27 08:11:00 +0100

    update powertools scripts

--- .vscode/addPoem.js
+++ .vscode/addPoem.js
@@ -1,7 +1,33 @@
 exports.execute = async (args) => {
-  const vscode = args.require('vscode');
+  const vscode  = args.require('vscode')
+  const slugify = require('slugify')
 
-  vscode.window.showInformationMessage(
-    args.replaceValues('Last recueil is ${currentRecueil} and current time is ${currentTime}')
-  );
-};
+  vscode.window
+    .showInputBox({placeHolder: 'Title'})
+    .then(poemTitle => {
+      if (poemTitle) {
+        const edit = new vscode.WorkspaceEdit()
+        const poemURI = vscode.Uri.file(
+          vscode.workspace.workspaceFolders[0].uri.fsPath
+          + '/'
+          + args.replaceValues('${addPoemRecueilsDir}')
+          + args.replaceValues('${addPoemRecueil}')
+          + '/'
+          + slugify(poemTitle, {lower: true})
+          + '.md'
+        )
+
+        edit.createFile(poemURI, {ignoreIfExists: true})
+        vscode.workspace.applyEdit(edit)
+
+        vscode.window.showInformationMessage('' + poemURI)
+
+        // boucle notif si fichier existe déjà > input slug (suggère "...-2/3/... .md")
+        // prefill file > workspace.fs.writeFile
+        // event listener onDidCreateFiles > open file > https://stackoverflow.com/a/39183552/6430293 > showInformationMessage
+        // vscode.window.showInformationMessage(vscode.window.activeTextEditor.document.uri + ' created')
+        // > but uri from workspace root
+        // gestion des erreurs
+      }
+  })
+}
